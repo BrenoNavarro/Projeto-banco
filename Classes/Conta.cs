6db4c418cba6673c2e0848bb1a84d161939fceb8
Breno Navarro <breno.navarro@yahoo.com.br>
2021-03-21 19:15:34 -0300

    projeto pronto

---
+++ Classes/Conta.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace dotnet-bank.Conta
+{
+    public class Conta
+    {
+        //Atributos
+        private TipoConta TipoConta {get; set;};
+        private double Saldo {get; set;};
+        private double Credito {get; set;};
+        private string Nome {get; set;};
+    
+        //Métodos
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnet_bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conta MinhaConta = new Conta();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
